b53aa1c5c1f83fd8bacd984323e6c670724ee9e4
Rasmi Jati <jatirasmi65@gmail.com>
2023-08-15 13:12:23 +0545

    created database with entity framework

---
+++ Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Models;
+
+namespace Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+        //create the database so we have create a dbset inside the applicationdbcontext
+        public DbSet<Category> Categories { get; set; }   //Categories --> table name with four column i.e. mentioned in our category model
+    
+    }
+}
